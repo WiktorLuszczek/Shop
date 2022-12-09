da2add5c79d5684248a0e59cfda841fecf499757
Wiktor Łuszczek <wiktoruszczek@gmail.com>
2022-12-09 11:49:55 +0100

    Add product page

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -7,8 +7,8 @@ export function Card (props: {data: SchemaProduct}) {
         <Link href={`product/${props.data.slug}`}>
             <section className=" hover:bg-gray-100 p-5 rounded-xl">
                 <Image src={props.data.images[0].url} alt={"product photo"} width={400} height={400}></Image>
-                <div className="grid grid-cols-2 grid-flow-row-dense font-medium">
-                    <p className="col-span-1">{props.data.name}</p>
+                <div className="grid grid-cols-3 font-medium">
+                    <p className="col-span-2">{props.data.name}</p>
                     <p className="text-right">{props.data.price}$</p>
                     <p className="font-light text-gray-500">{props.data.categories[0].name}</p>
                 </div>
--- pages/product/[...slug].tsx
+++ pages/product/[...slug].tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@apollo/client"
 import { useRouter } from "next/router"
 import { GET_PRODUCTS } from "../../lib/apollo-client";
 import type { SchemaProduct } from "../../schema/schema";
+import Image from "next/image";
 
 export default function Product () {
     const query = useQuery(GET_PRODUCTS)
@@ -10,12 +11,23 @@ export default function Product () {
     else{
         const slug = router.query.slug[0] // nie wiem jak dostać się inaczej :D
         const {products} = query.data;
-        const product = products.filter((product: SchemaProduct) => product.slug === slug)
+        const product = products.find((product: SchemaProduct) => product.slug === slug)
+        const addProduct = (e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault()
+            console.log("add to card")
+        }
         return(
-            <>
-                <h1>Name: {product[0].name}</h1>
-                <p>Price: {product[0].price}</p>
-            </>
+            <div className="grid grid-cols-3 gap-5">
+                <Image className="bg-gray-100 px-5 py-10 col-span-2 rounded-lg m-auto" src={product.images[0].url} alt="product photo"  width={600} height={600}></Image>
+                <div className="py-7">
+                    <p className="text-5xl font-medium tracking-wide my-3">{product.name}</p>
+                    <p className="text-xl my-3">{product.categories[0].name}</p>
+                    <p className="text-xl my-3">{product.price} $</p>
+                    <form onSubmit={(e) => addProduct(e)}>
+                        <input className="w-full bg-black text-white py-4 rounded-3xl text-xl font-medium my-5" type="submit" id="submit" value={"Add to cart"} />
+                    </form>
+                </div>
+            </div>
         )
     }
 }
