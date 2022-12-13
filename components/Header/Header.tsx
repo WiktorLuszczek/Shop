e834f28232849ad8fdf1e971e122a0987d9b5b8f
Wiktor Łuszczek <wiktoruszczek@gmail.com>
2022-12-13 18:27:59 +0100

    add order card

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import Script from "next/script";
+import { useState } from "react";
 import { SearchInput } from "../SearchInput/SearchInput";
 
 export function Header () {
+    const [hidden, setHidden] = useState('hidden')
+    const toggleOrderBox = () => {
+        setHidden(hidden === 'hidden' ? '' : 'hidden')
+    }
     return (
         <>
             <Script src="https://kit.fontawesome.com/0912d2c3f4.js" crossOrigin="anonymous"></Script>
@@ -20,6 +25,14 @@ export function Header () {
                     <i className="mx-1 text-5xl fa-brands fa-instagram"></i>
                     <i className="mx-1 text-5xl fa-brands fa-twitter"></i>
                     <i className="mx-1 text-5xl fa-brands fa-youtube"></i>
+                </div>
+                <div className="ml-20">
+                    <button onClick={toggleOrderBox}>
+                        <i id="toggle-button-baskets" className="text-5xl fa-regular fa-basket-shopping hover:text-gray-500"></i>
+                    </button>
+                </div>
+                <div className={`${hidden} absolute w-96 h-3/4 bg-white top-36 right-2 opacity-95 border-2 border-gray-400 rounded-l`}>
+                    <h1 className="text-center m-4 text-2xl">Products in the basket</h1>
                 </div>
             </header>
         </>
--- pages/product/[...slug].tsx
+++ pages/product/[...slug].tsx
@@ -14,6 +14,7 @@ export default function PageProduct () {
     const {products} = data;
     const product = products.find((product: SchemaProduct) => product.slug === slug[0])
     if(product === undefined) return <Spinner />
+    console.log(product)
     return(
         <Product product={product} />
     )
