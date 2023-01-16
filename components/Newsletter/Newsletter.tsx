7d5e3450b9ebbb151b3d6ab27479418d9d03a43c
Wiktor Łuszczek <wiktoruszczek@gmail.com>
2023-01-16 14:38:51 +0100

    HTML code refactoring

--- components/Newsletter/Newsletter.tsx
+++ components/Newsletter/Newsletter.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useState } from "react";
+import React, { HtmlHTMLAttributes, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { formSchema } from "../../schema/schema";
@@ -13,21 +13,21 @@ export const Newsletter = () => {
   } = useForm<{email: string}>({
     resolver: yupResolver(formSchema),
   });
-  const onSubmit = (data: {email: string}) => {
-    console.log(data)
+  const onSubmit = ({email}:  {email: string}) => {
+    console.log('email to newsletter', email)
     setShowModal(true)
   }
   return (
     <>
-    {showModla && <Modal action={setShowModal} text="You are add to newsletter"/>}
-    <div className="text-center">
-      <div className="text-xl">Join our Newsletter</div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input className="text-black block mx-auto m-1 px-2 rounded-lg" type="text" placeholder="Email..." {...register("email")} ></input>
-          <span className="block">{errors?.email?.message}</span>
-          <input className="cursor-pointer bg-gray-50 text-black px-2 rounded-lg mt-2" type="submit" id="submit" value="Subscribe!" ></input>
-        </form>
-    </div>
+      {showModla && <Modal action={setShowModal} text="You are add to newsletter"/>}
+      <div className="text-center">
+        <div className="text-xl">Join our Newsletter</div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input className="text-black block mx-auto m-1 px-2 rounded-lg" type="text" placeholder="Email..." {...register("email")} ></input>
+            <span className="block">{errors?.email?.message}</span>
+            <input className="cursor-pointer bg-gray-50 text-black px-2 rounded-lg mt-2" type="submit" id="submit" value="Subscribe!" ></input>
+          </form>
+      </div>
     </>
   );
 };
