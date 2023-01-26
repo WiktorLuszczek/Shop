71718a22e6e993d7f39e5476dbc0fb4ead0794c8
Wiktor Łuszczek <wiktoruszczek@gmail.com>
2023-01-26 12:52:27 +0100

    improving view of the page

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 export function Layout({ children }: { readonly children: React.ReactNode }) {
   return (
-    <div className="my-5">
+    <div className="my-5 max-w-maxWidth mx-auto">
       <div className="w-4/5 min-w-minWidth max-w-maxWidth my-12 mx-auto px-10">
         <div className="w-full">{children}</div>
       </div>
