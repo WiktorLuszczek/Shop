baec46afb4e0b9762e5f05c163f3fd819057b101
Wiktor Łuszczek <wiktoruszczek@gmail.com>
2023-01-26 13:05:12 +0100

    refactoring html code

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 export function Layout({ children }: { readonly children: React.ReactNode }) {
   return (
-    <div className="my-12 min-w-minWidth max-w-maxWidth mx-auto px-16">
+    <main className="my-12 min-w-minWidth max-w-maxWidth mx-auto px-16">
         {children}
-    </div>
+    </main>
   );
 }
